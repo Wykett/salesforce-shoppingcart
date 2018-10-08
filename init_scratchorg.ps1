8c7628e4bcdeb17ded15a8b1a3e218034696fecf
Wykett <bruno.chami@gmail.com>
2018-10-08 09:07:02 +0800

    Adding discount

--- init_scratchorg.ps1
+++ init_scratchorg.ps1
@@ -1,4 +1,4 @@
 param([string]$alias = (Get-Date).tostring("yyyyMMddhhmmss"))
 sfdx force:org:create -f config/project-scratch-def.json --setalias $alias --setdefaultusername --wait 2
 sfdx force:source:push
-sfdx force:user:permset:assign --permsetname Scratch
+sfdx force:user:permset:assign --permsetname AllfieldsPermissionSet
